9c60889a1487795fabeb45411b5f31292f3141f4
Aaron Tavistock <aaron@geekshow.com>
2024-06-18 20:31:35 -0700

    Readme expansion

--- lib/memory_baseline.ex
+++ lib/memory_baseline.ex
@@ -1,6 +1,6 @@
 defmodule ModuleBaseline do
-  @map Enum.into(1..1_000_000, %{}, fn n -> {"#{n}", n} end)
+  @map Enum.into(1..1_000_000, %{}, fn n -> {
+    Integer.to_string(n, 36), "a#{n}"
+  } end)
   def map, do: @map
 end
-
-IO.puts :erlang.memory(:total)
--- lib/memory_bloat.ex
+++ lib/memory_bloat.ex
@@ -1,10 +1,10 @@
 
 defmodule ModuleBloat do
-  @map Enum.into(1..1_000_000, %{}, fn n -> {"#{n}", n} end)
+  @map Enum.into(1..1_000_000, %{}, fn n -> {
+    Integer.to_string(n, 36), "a#{n}"
+  } end)
   def map, do: @map
   def get(key), do: Map.get(@map, key)
   def has_key?(key), do: Map.has_key?(@map, key)
   def keys, do: Map.keys(@map)
 end
-
-IO.puts :erlang.memory(:total)
